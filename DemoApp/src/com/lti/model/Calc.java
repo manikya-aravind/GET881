d8beca1dd75fc39f4ff3fa4bae17dd622fc5b870
manikya-aravind <manikyaaravind@gmail.com>
2020-12-17 17:55:56 +0530

    add multiply

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -7,5 +7,9 @@ public class Calc {
 	public void subs(int num1,int num2){
 		System.out.println("Sub = "+(num1-num2));
 	}
+	
+	public void mul(int num1,int num2){
+		System.out.println("mul = "+(num1*num2));
+	}
 
 }
