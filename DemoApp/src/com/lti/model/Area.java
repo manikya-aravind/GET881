375753577bb6a688680edbaac989adf859be9c69
manikya-aravind <manikyaaravind@gmail.com>
2020-12-18 15:35:27 +0530

    removed steps

--- DemoApp/src/com/lti/model/Area.java
+++ DemoApp/src/com/lti/model/Area.java
@@ -4,8 +4,6 @@ public class Area {
 	public void area(int num1,int num2){
 		System.out.println(num1*num2);
 	}
-	public void area1(int num1, double num2){
-		System.out.println(num1*num2);
-	}
+	
 
 }
