50d60628dc6f4e0b330427d387facc319b5b77d6
manikya-aravind <manikyaaravind@gmail.com>
2020-12-18 13:44:48 +0530

    added area1 in area class

--- DemoApp/src/com/lti/model/Area.java
+++ DemoApp/src/com/lti/model/Area.java
@@ -4,5 +4,8 @@ public class Area {
 	public void area(int num1,int num2){
 		System.out.println(num1*num2);
 	}
+	public void area1(int num1, double num2){
+		System.out.println(num1*num2);
+	}
 
 }
